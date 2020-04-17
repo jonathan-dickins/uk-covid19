902ec5cf9e5dc6e5a4ec52a0b6ebd0c3e32b36f1
Jonathan Dickins <42931047+jonathan-dickins@users.noreply.github.com>
2020-04-17 21:32:14 +0100

    Update coronauk3.R

--- coronauk3.R
+++ coronauk3.R
@@ -1,19 +1,4 @@
-install.packages("mapdata")
-install.packages("maptools")
-install.packages("rgdal")
-install.packages("ggmap")
-install.packages("rgeos")
-install.packages("broom")
-install.packages("plyr")
-install.packages("anchors")
-install.packages("viridis")
-install.packages("cowplot")
-install.packages("ggforce")
-
-
-#Clear the memory
-rm(list=ls())
-#Download some important packages
+# packages
 library(maps)
 library(mapdata)
 library(maptools)
@@ -31,16 +16,24 @@ library(dplyr)
 library(cowplot)
 library(ggforce)
 
+# this code work for a single day's cases. Download the cases data and format it as in test.csv in this repo
+# cases data is here https://coronavirus.data.gov.uk/
+
+# we'll also need population data by unitary authority- uapop.csv, also in this repo
+
+# edit some unitary authority names in the test and uapop files. They need to
+# correspond to the names in the map data.
 test$id <-revalue(test$id, c("Bournemouth, Christchurch and Poole" = "Bournemouth"))
 test$id <-revalue(test$id, c("Cornwall and Isles of Scilly" = "Cornwall"))
 uapop$id <-revalue(uapop$id, c("Bournemouth, Christchurch and Poole" = "Bournemouth"))
 
-#combine cases data with uapop data
+# combine cases data with uapop data
 newdf <- join(test, uapop, by = "id")
+
+# create cases per 100000 metric
 newdf$csht <- (newdf$cases/newdf$Estimated.Population.mid.2018)*100000
 
-#set up qua?tiles and fills
-
+# set up quantiles and fills for the bivariate map.
 {quantiles_cases <- newdf %>%
     pull(csht) %>%
     quantile(probs = seq(0, 1, length.out = 4), na.rm=TRUE)
@@ -49,6 +42,7 @@ newdf$csht <- (newdf$cases/newdf$Estimated.Population.mid.2018)*100000
     pull(X2018.people.per.sq..km) %>%
     quantile(probs = seq(0, 1, length.out = 4), na.rm=TRUE)
   
+ #assigning colours manually
   bivariate_color_scale <- tibble(
     "3 - 3" = "#3F2949", # high inequality, high income
     "2 - 3" = "#435786",
@@ -75,49 +69,51 @@ newdf$csht <- (newdf$cases/newdf$Estimated.Population.mid.2018)*100000
         breaks = quantiles_pop,
         include.lowest = TRUE
       ),
-      # by pasting the factors together as numbers we match the?groups defined
-      # in the tibble bivariate_color_scale
+    # by pasting the factors together as numbers we match the?groups defined
+    # in the tibble bivariate_color_scale
       group = paste(
         as.numeric(cases_quantiles), "-",
         as.numeric(pop_quantiles)
       )
     ) %>%
-    # we now join the actual hex values per "group"
-    # so each municipality knows its hex value?based on the his gini and avg
-    # income value
+    # we now join the actual hex values by "group"
+    # so each unitary authority is assigned its colour
     left_join(bivariate_color_scale, by = "group")
 }
 
+# we can drop the group variable
 newdf$group <- NULL
 
-#Load the shapefile - make sure you change the filepath to where you saved the shapefiles
-shapefile <- readOGR(dsn="C:/Users/Jonathan/Downloads/CUAs", layer="Counties_and_Unitary_Authorities_December_2017_Full_Extent_Boundaries_in_UK_WGS84")
+# Load the shapefile - make sure you change the filepath to where you saved the shapefiles
+# shapefile is in the repo
+# PHE reports Covid-19 cases by upper tier local authority- a mix of counties,
+# metropolitan areas, and London boroughs. 
+shapefile <- readOGR(dsn="filepath", layer="Counties_and_Unitary_Authorities_December_2017_Full_Extent_Boundaries_in_UK_WGS84")
 str(shapefile)
+
 #Reshape for ggplot2 using the Broom package
 mapdata <- tidy(shapefile, region="ctyua17nm") #This might take a few minutes
 
-#Check the s?apefile has l?aded correctly by plotting an outline map of the UK
+#Check the shapefile has loaded correctly by plotting an outline map of the UK
 gg <- ggplot() + geom_polygon(data = mapdata, aes(x = long, y = lat, group = group), color = "#FFFFFF", size = 0.25)
-gg <- gg + coord_fixed(1) #This gives the map a 1:1 aspect ratio to preve?t the map fro? appearing squashed
+gg <- gg + coord_fixed(1) #This gives the map a 1:1 aspect ratio to prevent the map from appearing squashed
 print(gg)
 
-#some data cleaning
-unique_map <- as.data.frame(unique(mapdata$id))
-unique_test <- as.data.frame(unique(test$id))
-
+# changing some name values so they match the uapop and test files
 mapdata$id <-revalue(mapdata$id, c("Hackney" = "Hackney and City of London"))
 mapdata$id <-revalue(mapdata$id, c("City of London" = "Hackney and City of London"))
 
 #Join newdf  with mapdata
 df <- join(mapdata, newdf, by="id")
 
-#Create the heatmap using the ggplot2 package
+#PLOTTING
+#The bivariate map
 gg <- ggplot() + geom_polygon(data = df, 
                               aes(x = long, y = lat, group = group, fill = fill), 
                               color = "#FFFFFF", 
                               size = 0.25)
-gg <- gg + scale_fill_identity()
-gg <- gg + coord_fixed(ylim = c(49.5, 56), xlim = c(-7, 1.4), ratio = 1.3)##(ylim = c(51.25, 51.75),?xlim=-c(-0.75, 0.65), ratio=1.3)
+gg <- gg + scale_fill_identity() #use scale_fill_identity because the fill is provided by the hexcodes
+gg <- gg + coord_fixed(ylim = c(49.5, 56), xlim = c(-7, 1.4), ratio = 1.3)
 gg <- gg + theme_minimal()
 gg <- gg + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 gg <- gg + theme(axis.title.x=element_blank(), axis.text.x = element_blank(), axis.ticks.x = element_blank())
@@ -132,6 +128,7 @@ gg <- gg + labs(x = NULL,
 
 print(gg) 
 
+#London box
 ldn <- ggplot() + geom_polygon(data = df, 
                                aes(x = long, y = lat, group = group, fill = fill), 
                                color = "#FFFFFF", 
@@ -145,7 +142,7 @@ ldn <- ldn + theme(axis.title.y=element_blank(), axis.text.y = element_blank(), 
 ldn <- ldn + theme(legend.position = "bottom")
 print(ldn) 
 
-# LEGEND
+# Legend
 bivariate_color_scale %<>%
    separate(group, into = c("cases", "pop"), sep = " - ") %>%
   mutate(cases = as.integer(cases),
@@ -173,15 +170,13 @@ axis.text = element_blank()
   # quadratic tiles
   coord_fixed()
 
-str(df)
-#draw plot
+#Final figure
 ggdraw() +
   draw_plot(gg, 0, 0, 1, 1) +
   draw_plot(legend, 0.075, 0.2, 0.3, 0.3) +
   draw_plot(ldn, 0.075, 0.6, 0.4, 0.3)
 
-str(df)
-#univariate
+#a univariate map for cases
 uni <- ggplot() + geom_polygon(data = df, 
                                aes(x = long, y = lat, group = group, fill = csht), 
                                color = "#FFFFFF", 
@@ -238,14 +233,7 @@ ggdraw() +
   draw_plot(uni, 0, 0, 1, 1) +
   draw_plot(ldn2, 0.075, 0.6, 0.4, 0.3)
 
-
-ggdraw() +
-  draw_plot(gg, 0, 0, 1, 1) +
-  draw_plot(ldn, 0.075, 0.6, 0.3, 0.2) +
-  draw_plot(legend, 0.075, 0.2, 0.3, 0.3) 
-
-str(df)
-
+#scatterplot
 stplt <- ggplot(newdf, 
                 aes(x = X2018.people.per.sq.km, y = csht, colour = fill)) + geom_point(size = 4) + scale_colour_identity() +
   xlim(0, 18000) + ylim(-3, 120) + theme_minimal() + 
@@ -261,23 +249,3 @@ stplt <- ggplot(newdf,
 ggdraw() +
   draw_plot(stplt, 0, 0, 1, 1) +
   draw_plot(legend, 0.075, 0.2, 0.3, 0.3)
-
-
-
-
-
-#annotating
-gg <- gg + 
-  annotate(
-    geom = "curve", x = -1.5, y = 50, xend = -1.3, yend = 51, 
-    curvature = .3, arrow = arrow(length = unit(2, "mm"))
-  ) +
-  annotate(geom = "text", x = -1.4, y = 50, label = "Hampshire", hjust = "left")
-print(gg)
-
-midpoints <- mapdata %>%
-  group_by(id) %>%
-  summarise(midlat = mean(lat), midlong = mean(long))
-
-df<- left_join(df, midpoints, by = "id")
-
